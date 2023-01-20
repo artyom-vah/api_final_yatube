16773d30f00db7ed993b865eaa695e2929b95097
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-01-20 14:31:57 +0500

    8 коммит

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,13 +7,13 @@ app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'follow', FollowViewSet, basename='follow')
 router.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-router.register(r'groups', GroupViewSet, basename='groups')
-router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include('djoser.urls')),
