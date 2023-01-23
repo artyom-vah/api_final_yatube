3f597747fb28def902bc68e7fce95310ffc9e8eb
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-01-23 11:35:51 +0500

    8 коммит

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -28,16 +28,25 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
-    def get_post_id(self):
-        return self.kwargs.get('post_id')
+    # def get_post_id(self):
+    #     return self.kwargs.get('post_id')
+
+    # def perform_create(self, serializer):
+    #     post = get_object_or_404(Post, id=self.get_post_id())
+    #     serializer.save(author=self.request.user, post=post)
+
+    # def get_queryset(self):
+    #     post = get_object_or_404(Post, id=self.get_post_id())
+    #     return post.comments.all()
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.get_post_id())
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.get_post_id())
-        return post.comments.all()
+        return self.get_post().comments.all()
 
 
 class FollowViewSet(mixins.CreateModelMixin,
